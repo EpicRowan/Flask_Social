1dfa4610fb1974645c5d37f2a2eb1a9dc3fc3509
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-06-10 16:59:23 -0700

    Set up database schema

---
+++ schema.sql
@@ -0,0 +1,31 @@
+
+drop table if exists posts;
+	create table posts (
+		id integer primary key autoincrement,
+		name text not null,
+		content text not null
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
