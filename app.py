210460dac72ec80f0777990b051f4ef0589e73c0
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-06-10 17:03:42 -0700

    Created function and route for homepage

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 app.secret_key = "supersecret"
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	if request.method == 'GET':
+		pass
+	if request.mthod == 'POST':
+		request.form.get('name')
+		request.form.get('post')
+
+	return render_template('index.html')
 
 if __name__ == "__main__":
 	app.run(debug=True, port=5000, host='0.0.0.0')
