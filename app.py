cf022d5d5578dd70679fafc296b9365b6b90119c
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-06-10 17:09:24 -0700

    Initialized model.py file and created create_post function

--- app.py
+++ app.py
@@ -8,9 +8,10 @@ app.secret_key = "supersecret"
 def index():
 	if request.method == 'GET':
 		pass
-	if request.mthod == 'POST':
-		request.form.get('name')
-		request.form.get('post')
+	if request.method == 'POST':
+		name = request.form.get('name')
+		post = request.form.get('post')
+		create_post(name, post)
 
 	return render_template('index.html')
 
