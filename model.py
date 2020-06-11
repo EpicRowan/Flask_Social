310c96f7764b8fd5607a212897b96db2a69b1e6f
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-06-10 17:19:03 -0700

    Created index.html wth initial html

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ ROOT = path.dirname(path.relpath(__file__))
 def create_post(name, content):
 	con = sql.connect(path.join(ROOT, 'database.db'))
 	cur = con.cursor()
-	cur.execute('insert into posts(name, content) values(?,?)', (name, content)
+	cur.execute('insert into posts(name, content) values(?,?)', (name, content))
 	con.commit()
 	con.close()
 
